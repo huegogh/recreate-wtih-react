1c4263850bc6d11b16b4063bf1c189b5299081f4
huegogh <huegogh42@gmail.com>
2021-01-20 23:03:23 -0800

    add ability to change navbar with react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,25 +3,18 @@ import { Home } from './components/Home.js';
 import { About } from './components/About.js';
 import { Contact } from './components/Contact.js';
 import { NoMatch } from './components/NoMatch.js';
-import { Layout } from './components/Layout.js';
-import { NavigationBar } from './components/NavigationBar.js';
-import { Jumbo } from './components/Jumbo.js';
 import React from 'react';
 
 function App() {
   return (
     <React.Fragment>
       <Router>
-        <NavigationBar />
-        <Jumbo />
-        <Layout>
           <Switch>
             <Route exact path='/' component={Home} />
             <Route path='/about' component={About} />
             <Route path='/contact' component={Contact} />
             <Route component={NoMatch} />
           </Switch>
-        </Layout>
       </Router>
     </React.Fragment>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,14 @@
-export function About () {
+import React from 'react';
+import { PageTemplate } from './PageTemplate.js';
+
+export function About() {
     return (
-        <div>
+        <PageTemplate>
             <h2>About Page</h2>
             <p>This  is the first paragraph</p>
             <p>This  is the second paragraph</p>
             <p>This  is the third paragraph</p>
-        </div>
+        </PageTemplate>
     );
 }
+
