d18809f5d947fc9fc47856ac11c571d9891268c4
huegogh <huegogh42@gmail.com>
2021-02-13 17:56:18 -0800

    initial commit

---
+++ src/components/hotel-components/MyNavbar.js
@@ -0,0 +1,16 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+
+export function MyNavbar(props) {
+    return(
+        <Row>
+            <Col>
+            <Navbar expand='md'>
+
+
+            </Navbar>
+            </Col>
+        </Row>
+    );
+}
---
+++ src/components/multipage-components/GamesList.js
@@ -0,0 +1,234 @@
+import PGImage from '../../assets/multipage-assets/squareImages/Pokemon_Go.png';
+import ACNHImage from '../../assets/multipage-assets/squareImages/220px-Animal_Crossing_New_Horizons.jpg';
+import OkamiImage from '../../assets/multipage-assets/squareImages/okami.jpg';
+import SuperImage from '../../assets/multipage-assets/squareImages/Super_mario_sunshine.jpg';
+import PortalImage from '../../assets/multipage-assets/squareImages/220px-Portal_standalonebox.jpg';
+import DDRImage from '../../assets/multipage-assets/squareImages/200px-Dance_Dance_Revolution_Extreme_arcade_machine_left_side_stage.png';
+import BOTWImage from '../../assets/multipage-assets/squareImages/220px-The_Legend_of_Zelda_Breath_of_the_Wild.jpg';
+import MetroidImage from '../../assets/multipage-assets/squareImages/220px-MetroidPrimebox.jpg';
+import SMWImage from '../../assets/multipage-assets/squareImages/220px-Super_Mario_World_Coverart.png';
+import OriImage from '../../assets/multipage-assets/squareImages/Ori_and_the_Blind_Forest_Logo.jpg';
+
+
+export let GamesList = [{
+    number: 10,
+    title: 'Pokemon Go',
+    squareImage: PGImage,
+    about: `Pokémon Go is a 2016 augmented reality mobile game developed
+    and published by Niantic in collaboration with The Pokémon
+    Company for iOS and Android devices. A part of the Pokémon
+    franchise, the game is the result of a collaboration between
+    Niantic, Nintendo and The Pokémon Company. It uses the
+    mobile device GPS to locate, capture, battle, and train
+    virtual creatures, called Pokémon, which appear as if they
+    are in the player's real-world location. The game is
+    free-to-play; it uses a freemium business model and supports
+    in-app purchases for additional in-game items. The game
+    launched with around 150 species of Pokémon, which had
+    increased to around 600 by 2020.`,
+    videoLink: "https://www.youtube.com/embed/Qryf2F_QfXg",
+
+},
+{
+    number: 9,
+    title: 'Animal Crossing: New Horizons',
+    squareImage: ACNHImage,
+    about: `Animal Crossing: New Horizons is a life simulation video
+    game developed and published by Nintendo for the Nintendo
+    Switch. It is the fifth main series game in the Animal
+    Crossing series and was released on March 20, 2020. In New
+    Horizons, the player assumes the role of a customizable
+    character who moves to a deserted island after purchasing a
+    package from Tom Nook, a tanuki character who has appeared
+    in every entry in the Animal Crossing series. Taking place
+    in real-time, the player can explore the island in a
+    nonlinear fashion, gathering and crafting items, catching
+    insects and fish, and developing the island into a community
+    of anthropomorphic animals. faucibus.`,
+    videoLink: "https://www.youtube.com/embed/_3YNL0OWio0",
+
+},
+{
+    number: 8,
+    title: 'Okami',
+    squareImage: OkamiImage,
+    about: `Ōkami is an action-adventure video game developed by Clover
+    Studio and published by Capcom. It was released for
+    PlayStation 2 in 2006 in Japan and North America, and in
+    2007 in Europe and Australia. After the closure of Clover
+    Studio a few months after the release, a port for Wii was
+    developed by Ready at Dawn, Tose, and Capcom, and released
+    in 2008. Set in classical Japan, Ōkami combines Japanese
+    mythology and folklore to tell the story of how the land was
+    saved from darkness by the Shinto sun goddess, named
+    Amaterasu, who took the form of a white wolf. It features a
+    sumi-e-inspired cel-shaded visual style and the Celestial
+    Brush, a gesture-system to perform miracles. The game was
+    planned to use more traditional realistic rendering, but
+    this had put a strain on the graphics processing of the
+    PlayStation 2. Clover Studio switched to a cel-shaded style
+    to reduce the processing, which led to the Celestial Brush
+    concept. The gameplay is modeled on The Legend of Zelda, one
+    of director Hideki Kamiya's favorite series.`,
+    videoLink: "https://www.youtube.com/embed/2w6xtBVc6NM",
+
+},
+{
+    number: 7,
+    title: 'Super Mario Sunshine',
+    squareImage: SuperImage,
+    about: `Super Mario Sunshine is a 2002 platform game developed and
+    published by Nintendo for the GameCube. It is the second 3D
+    platformer in the Super Mario series, following Super Mario
+    64 (1996). The game was directed by Yoshiaki Koizumi and
+    Kenta Usui, produced by series creators Shigeru Miyamoto and
+    Takashi Tezuka, written by Makato Wada, and scored by Koji
+    Kondo and Shinobu Tanaka. The game takes place on the
+    tropical Isle Delfino, where Mario, Toadsworth, Princess
+    Peach and five Toads are taking a vacation. A villain
+    resembling Mario, known as Shadow Mario, vandalizes the
+    island with graffiti and leaves Mario to be wrongfully
+    convicted for the mess. Mario is ordered to clean up Isle
+    Delfino, using a device called the Flash Liquidizer Ultra
+    Dousing Device (F.L.U.D.D.), while saving Princess Peach
+    from Shadow Mario.`,
+    videoLink: "https://www.youtube.com/embed/0dT3YjdSxH4",
+},
+{
+    number: 6,
+    title: 'Portal',
+    squareImage: PortalImage,
+    about: `Portal is a 2007 puzzle-platform game developed and
+    published by Valve. It was released in a bundle, The Orange
+    Box, for Windows, Xbox 360 and PlayStation 3, and has been
+    since ported to other systems, including Mac OS X, Linux,
+    and Android (via Nvidia Shield). Portal consists primarily
+    of a series of puzzles that must be solved by teleporting
+    the player's character and simple objects using "the
+    Aperture Science Handheld Portal Device", a device that can
+    create inter-spatial portals between two flat planes. The
+    player-character, Chell, is challenged and taunted by an
+    artificial intelligence named GLaDOS (Genetic Lifeform and
+    Disk Operating System) to complete each puzzle in the
+    Aperture Science Enrichment Center using the portal gun with
+    the promise of receiving cake when all the puzzles are
+    completed. The game's unique physics allows kinetic energy
+    to be retained through portals, requiring creative use of
+    portals to maneuver through the test chambers. This gameplay
+    element is based on a similar concept from the game
+    Narbacular Drop; many of the team members from the DigiPen
+    Institute of Technology who worked on Narbacular Drop were
+    hired by Valve for the creation of Portal, making it a
+    spiritual successor to the game.`,
+    videoLink: "https://www.youtube.com/embed/TluRVBhmf8w",
+},
+{
+    number: 5,
+    title: 'Dance Dance Revolution',
+    squareImage: DDRImage,
+    about: `Dance Dance Revolution is a music video game series produced
+    by Konami. Introduced in Japan in 1998 as part of the Bemani
+    series, and released in North America and Europe in 1999,
+    Dance Dance Revolution is the pioneering series of the
+    rhythm and dance genre in video games. Players stand on a
+    "dance platform" or stage and hit colored arrows laid out in
+    a cross with their feet to musical and visual cues. Players
+    are judged by how well they time their dance to the patterns
+    presented to them and are allowed to choose more music to
+    play to if they receive a passing score.`,
+    videoLink: "https://www.youtube.com/embed/U8boOEQZUj0",
+},
+{
+    number: 4,
+    title: 'The Legend of Zelda: Breath of the Wild',
+    squareImage: BOTWImage,
+    about: `The Legend of Zelda: Breath of the Wild is a 2017
+    action-adventure game developed and published by Nintendo
+    for the Nintendo Switch and Wii U consoles. Breath of the
+    Wild is part of the Legend of Zelda franchise and is set at
+    the end of the Zelda timeline; the player controls Link, who
+    awakens from a hundred-year slumber to defeat Calamity Ganon
+    and save the kingdom of Hyrule. Similar to the original
+    Legend of Zelda (1986), players are given little instruction
+    and can explore the open world freely. Tasks include
+    collecting multipurpose items to aid in objectives or
+    solving puzzles and side quests for rewards. The world is
+    unstructured and designed to reward experimentation, and the
+    story can be completed in a nonlinear fashion.`,
+    videoLink: "https://www.youtube.com/embed/zw47_q9wbBE",
+},
+{
+    number: 3,
+    title: 'Metroid Prime',
+    squareImage: MetroidImage,
+    about: `Metroid Prime is an action-adventure game developed by Retro
+    Studios and Nintendo for the GameCube. Metroid Prime is the
+    fifth main installment in the Metroid series, and the first
+    Metroid game to use 3D computer graphics being played from
+    the first-person perspective. Because exploration takes
+    precedence over combat, Nintendo classifies the game as a
+    first-person adventure rather than a first-person shooter.
+    It was released in North America in November 2002, and in
+    Japan and Europe the following year. On the same day as its
+    North American release, Nintendo also released the Game Boy
+    Advance game Metroid Fusion, marking the return of the
+    Metroid series after an eight-year hiatus following Super
+    Metroid (1994). Metroid Prime is the first of the three-part
+    Prime storyline, which takes place between the original
+    Metroid and Metroid II: Return of Samus. Like previous games
+    in the series, Metroid Prime has a science fiction setting
+    in which players control the bounty hunter Samus Aran. The
+    story follows Samus as she battles the Space Pirates and
+    their biological experiments on the planet Tallon IV. The
+    game was a collaboration between Retro's staff in Austin,
+    Texas, and Japanese Nintendo employees, including producer
+    Shigeru Miyamoto, who suggested the project after visiting
+    Retro's headquarters in 2000.`,
+    videoLink: "https://www.youtube.com/embed/hwW8fpECZn8",
+},
+{
+    number: 2,
+    title: 'Super Mario World',
+    squareImage: SMWImage,
+    about: `Super Mario World is a 1990 platform game developed by
+    Nintendo for the Super Nintendo Entertainment System. The
+    story follows Mario's quest to save Princess Toadstool and
+    Dinosaur Land from the series antagonist Bowser and his
+    minions, the Koopalings. The gameplay is similar to that of
+    earlier Super Mario games: Players control Mario or his
+    brother Luigi through a series of levels in which the goal
+    is to reach the flagpole at the end. Super Mario World
+    introduced Yoshi, a dinosaur who can eat enemies, as well as
+    gain abilities by eating the shells of Koopa Troopas.
+    Nintendo Entertainment Analysis & Development developed the
+    game, led by director Takashi Tezuka and producer and series
+    creator Shigeru Miyamoto. It is the first Mario game for the
+    SNES and was designed to make the most of the console's
+    technical features. The development team had more freedom
+    compared to the series installments for the Nintendo
+    Entertainment System (NES). Yoshi was conceptualised during
+    the development of the NES games but was not used until
+    Super Mario World due to hardware limitations. Super Mario
+    World is often considered one of the greatest video games of
+    all time.`,
+    videoLink: "https://www.youtube.com/embed/WfnVoapbEHk",
+},
+{
+    number: 1,
+    title: 'Ori and the Blind Forest',
+    squareImage: OriImage,
+    about: `Ori and the Blind Forest is a platform-adventure
+    Metroidvania video game developed by Moon Studios and
+    published by Microsoft Studios. The game was released for
+    Xbox One and Microsoft Windows on March 11, 2015 and for
+    Nintendo Switch on September 27, 2019. Players assume
+    control of Ori, a small white guardian spirit, and Sein, the
+    "light and eyes" of the Forest's Spirit Tree. Players are
+    tasked to move between platforms and solve puzzles. The game
+    features a save system called "Soul Links", which allows
+    players to save their progress at will with limited
+    resources, and an upgrade system that gives players the
+    ability to strengthen Ori's skills and abilities.`,
+    videoLink: "https://www.youtube.com/embed/cklw-Yu3moE",
+}
+]
