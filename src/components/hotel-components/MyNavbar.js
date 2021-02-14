b580ae96cffb59fc72e25e76c6c6b9c94d445747
huegogh <huegogh42@gmail.com>
2021-02-13 22:17:29 -0800

    add links and logo

--- src/components/hotel-components/MyNavbar.js
+++ src/components/hotel-components/MyNavbar.js
@@ -1,16 +1,48 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import styled from 'styled-components';
+import Logo from "../../assets/hotel-assets/Logo.png";
+
+const Styles = styled.div`
+    .bg-custom {
+        background-color: #000324;
+    }
+`;
 
 export function MyNavbar(props) {
-    return(
-        <Row>
-            <Col>
-            <Navbar expand='md'>
-
-
-            </Navbar>
-            </Col>
-        </Row>
+    return (
+        <Styles>
+            <Row>
+                <Col className="px-0">
+                    <Navbar
+                    variant="dark"
+                    className="bg-custom"
+                    expand="md">
+                        <Navbar.Brand className='d-md-none'>La Petite Maison</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse
+                            id="basic-navbar-nav">
+                            <Nav className="mx-auto d-flex align-items-end">
+                                <Nav.Link href="#overview">Overview</Nav.Link>
+                                <Nav.Link href="#rooms">Rooms</Nav.Link>
+                                <Nav.Link href="#packages">Packages</Nav.Link>
+                                <Nav.Link>
+                                    <img
+                                    src={Logo}
+                                    className="d-none d-md-block"
+                                    alt="Logo of La Petite Maison" />
+                                </Nav.Link>
+                                <Nav.Link href="#amenities">Amenities</Nav.Link>
+                                <Nav.Link href="#contact">Contact</Nav.Link>
+                                <Nav.Link href="#reserve">Reserve</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </Col>
+            </Row>
+        </Styles>
     );
 }
+
