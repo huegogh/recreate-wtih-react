780756f75c2b3ff8615ff85b7fd2e227e98e2b8f
huegogh <huegogh42@gmail.com>
2021-02-16 18:07:31 -0800

    add components

--- src/components/pages/Business.js
+++ src/components/pages/Business.js
@@ -1,7 +1,39 @@
 import { BackButton } from '../shared-components/BackButton.js';
+import { NavigationBar } from '../business-components/NavigationBar.js';
+import { Styles } from '../business-components/Styles.js';
+import { Route, useRouteMatch, Switch } from 'react-router-dom';
+import { Home } from '../business-components/Home.js';
+import { Classes } from '../business-components/Classes.js';
+import { MyAccount } from '../business-components/MyAccount.js';
+import { Contact } from '../business-components/Contact.js';
+import { NoMatch } from './NoMatch.js';
+import { Footer } from '../business-components/Footer.js';
+import { Password } from '../business-components/Password.js';
+
 
 export function Business() {
+    let { path } = useRouteMatch();
+
+    const NavLinks = [
+        { path: '', linkName: 'Home' },
+        { path: '/classes', linkName: 'Classes' },
+        { path: '/contact', linkName: 'Contact Us' },
+        { path: '/account', linkName: 'My Account' }
+    ];
+
     return (
-        <BackButton />
+        <Styles>
+            <NavigationBar content={NavLinks} />
+                <Switch>
+                    <Route exact path={path} component={Home} />
+                    <Route path={`${path}/classes`} component={Classes} />
+                    <Route path={`${path}/contact`} component={Contact} />
+                    <Route path={`${path}/account`} component={MyAccount} />
+                    <Route path={`${path}/password`} component={Password} />
+                    <Route component={NoMatch} />
+                </Switch>
+                <Footer />
+            <BackButton />
+        </Styles>
     );
 }
